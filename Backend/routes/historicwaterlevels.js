4d15c7a111cbcab37aca94b067aee0ae739e23f3
rebeccalindman <rebeccalindman@icloud.com>
2025-04-22 15:30:10 +0200

    waterlevels post

--- Backend/routes/historicwaterlevels.js
+++ Backend/routes/historicwaterlevels.js
@@ -1,9 +1,32 @@
 import { Router } from 'express';
+import historicwaterlevelsMockdata from '../data/mockdata/historicwaterlevels.json' assert { type: 'json' };
 
-const router = Router();
+const router = Router({ mergeParams: true });
 
 router.get('/', (req, res) => {
     res.send('historicwaterlevels');
 });
 
+// Filter historic waterlevels for _specific_ sensor by date
+/* router.get('/?from=:from&to=:to', (req, res) => {
+    const { sensorID } = req.params;
+    const { from, to } = req.query;
+    const sensorData = historicwaterlevelsMockdata.find(sensor => sensor.sensorID === sensorID);
+  
+    if (!sensorData) {
+      return res.status(404).json({ error: `Sensor with ID ${sensorID} not found.` });
+    }
+  
+    const filteredMeasurements = sensorData.measurements.filter(measurement => {
+      const measurementDate = new Date(`${measurement.date}T${measurement.time}`);
+      return measurementDate >= new Date(from) && measurementDate <= new Date(to);
+    });
+  
+    res.json({
+      sensorID: sensorData.sensorID,
+      measurements: filteredMeasurements
+    });
+  });
+
 export default router;
+ */
--- Backend/routes/waterlevels.js
+++ Backend/routes/waterlevels.js
@@ -18,4 +18,35 @@ router.get('/', (req, res) => {
   });
 });
 
+
+router.post('/', (req, res) => {
+  const { sensorID } = req.params;
+  const { waterlevel, date, time } = req.body;
+  const sensorData = waterlevelsMockdata.find(sensor => sensor.sensorID === sensorID);
+
+  if (!sensorData) {
+    return res.status(404).json({ error: `Sensor with ID ${sensorID} not found.` });
+  }
+
+  const newMeasurement = {
+    waterlevel,
+    date,
+    time
+  };  
+
+  sensorData.measurements.push(newMeasurement);   
+
+  res.json({
+    sensorID: sensorData.sensorID,
+    measurements: sensorData.measurements
+  });
+});
+
+router.delete('/:measurementID', (req, res) => res.json({
+  message: 'Measurement deleted',
+  data: {
+    measurementID: req.params.measurementID
+  }
+  }))
+
 export default router;

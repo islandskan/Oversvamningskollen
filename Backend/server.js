b70a622d753eb8be1583226a4e62fb40891d18fc
rebeccalindman <rebeccalindman@icloud.com>
2025-04-25 10:24:22 +0200

    installed morgan pkg and added more logs for get requests

--- Backend/server.js
+++ Backend/server.js
@@ -5,6 +5,8 @@ import userRouter from './routes/user.js';
 import errorHandler from "./middleware/errorHandler.js";
 import handleProcessEvents from "./startup/handleProcessEvents.js";
 import dotenv from 'dotenv';
+import morganMiddleware from './middleware/loggerMiddleware.js';
+
 dotenv.config();
 
 handleProcessEvents();
@@ -13,6 +15,9 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+
+// ✅ This logs all HTTP requests (GET, POST, etc.) in dev mode
+app.use(morganMiddleware);
 
 // Basic route
 app.get('/', (req, res) => {
@@ -23,10 +28,18 @@ app.get('/', (req, res) => {
 app.use('/api/sensors', sensorRouter);
 app.use('/api/users', userRouter);
 
+// undefined routes are logged
+app.use((req, res, next) => {
+  const message = `404 - Not Found - ${req.originalUrl}`;
+  logger.warn(message);
+  res.status(404).json({ error: message });
+});
+
 app.use(errorHandler);
 
 logger.info(`Logger initialized at level: ${logger.level}`);
 
 app.listen(PORT, () => {
   console.log(`Servern körs på http://localhost:${PORT}`);
+  logger.info(`Servern körs på http://localhost:${PORT}`);
 });

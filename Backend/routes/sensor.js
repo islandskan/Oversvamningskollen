553c0c9fa205d37e624cbec31e68e0e510890aaf
rebeccalindman <rebeccalindman@icloud.com>
2025-04-28 14:23:33 +0200

    updated sensor PATCH validation

--- Backend/routes/sensor.js
+++ Backend/routes/sensor.js
@@ -34,12 +34,12 @@ router.use('/:sensorID/historicwaterlevels', historicwaterlevelsRouter);
 
 //specific sensor
 router.get('/:sensorID', (req, res) => {
-  const id=req.params.id;
-  const sensor = sensors.find(u => u.id === id);
+  const id=req.params.sensorID;
+  const sensor = sensors.find(u => u.sensorID === id);
   if(!sensor){
     return res.status(404).json({message: 'Sensorn kan inte hittas'});
   }
-  res.status(200).json({ message: `Hämtar sensor ${sensor.id}`, sensor });
+  res.status(200).json({ message: `Hämtar sensor: ${sensor.sensorName}`, sensor });
 });
 
 //POST 
@@ -72,7 +72,7 @@ router.post('/', (req, res) => {
 
 //todo update battery status
 //todo update with sensor whitelist and fields
-const allowedUpdates = ['name', 'mail', 'password']; //* input whitelist
+const allowedUpdates = ['batteryStatus', 'installationDate', 'sensorName', 'locationID']; //* input whitelist
 
 router.patch('/:sensorID', (req, res) => {
   const id = req.params.sensorID;
@@ -84,11 +84,12 @@ router.patch('/:sensorID', (req, res) => {
   if (invalidFields.length > 0) { //validates and sends feedback to developer
     return res.status(400).json({ 
       error: 'Ogiltiga fält i uppdateringen',
-      invalidFields: invalidFields 
+      invalidFields: invalidFields,
+      validFields: allowedUpdates
     });
   }
 
-  const sensor = sensors.find(s => s.id === id);
+  const sensor = sensors.find(s => s.sensorID === id);
   if (!sensor) return res.status(404).send('Sensorn hittades inte');
 
   Object.assign(sensor, req.body);
@@ -103,6 +104,7 @@ router.delete('/:sensorID', (req, res) => {
   const id = req.params.sensorID;
 
   const index = sensors.findIndex(p => p.sensorID === id);
+  const sensorName = sensors[index].sensorName;
 
   if (index === -1) {
     return res.status(404).json({ message: 'Sensorn hittades inte'});
@@ -110,7 +112,7 @@ router.delete('/:sensorID', (req, res) => {
 
   sensors.splice(index, 1);
 
-  res.json({ message: `Tog bort en sensor med id: ${id}`, sensors });
+  res.json({ message: `Tog bort sensor: ${sensorName} med id: ${id}`, sensors });
 });
 
 

1dc918f429f8eba6d4e7f9a4ae8a6bddd46dd8f8
islandskan <sigridur.eggertsdottir@chasacademy.se>
2025-05-13 08:57:27 +0200

    add temporary return value atm

--- system/lora32/src/BatteryManager.cpp
+++ system/lora32/src/BatteryManager.cpp
@@ -5,9 +5,11 @@ BatteryManager::BatteryManager(bool mock) : mock_mode(mock) {}
 float BatteryManager::read_voltage() {
     if (mock_mode) {
         // return random value within range
+        return 80.0;
     }
     else {
         // read actual value from the analog pins
         // return actual value
     }
+    return 0.0;
 }

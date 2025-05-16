9f69eabc942edf224738f13fe069c70454f8ae6c
ToeCrow <thomas.kronvold@chasacademy.se>
2025-05-16 12:48:08 +0200

    created and connected to neon for db

--- Backend/server.js
+++ Backend/server.js
@@ -1,14 +1,11 @@
-// server.js
-import app from './app.js';  // Import app from app.js
+import app from './app.js';
 import dotenv from 'dotenv';
 
-
-dotenv.config(); // .env kommer nu att laddas från rotmappen automatiskt
-
-
+dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Servern körs på http://localhost:${PORT}`);
+  console.log(`Servern kör på http://localhost:${PORT}`);
+  console.log('Ansluten till DB:', process.env.PG_URI);
 });

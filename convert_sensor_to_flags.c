8964585465aa3e39c1dc68ca98798d42d23e76f2
Linus Nilsson <linussnilsson03@gmail.com>
2025-05-19 11:22:21 +0200

    feat(added more precise flags)

--- convert_sensor_to_flags.c
+++ convert_sensor_to_flags.c
@@ -10,17 +10,22 @@ typedef enum {
     BATTERY_LOW = 1u << 2, // 4
     LOST_COMMUNICATION = 1u << 3, // 8
     SENSOR_FAILURE = 1u << 4, // 16
-    THRESHOLD_ABOVE_20 = 1u << 5, // 32
-    THRESHOLD_ABOVE_30 = 1u << 6, // 64
-    THRESHOLD_ABOVE_40 = 1u << 7, // 128
-    THRESHOLD_ABOVE_50 = 1u << 8, // 256
-    RATE_OF_CHANGE_SMALL = 1u << 9, // 512
-    RATE_OF_CHANGE_MEDIUM = 1u << 10, // 1024
-    RATE_OF_CHANGE_LARGE = 1u << 11, // 2048
-    INTERNAL_FLAG_01 = 1u << 12, // 4096
-    INTERNAL_FLAG_02 = 1u << 13, // 8192
-    INTERNAL_FLAG_03 = 1u << 14, // 16384
-    INTERNAL_FLAG_04 = 1u << 15, // 32768
+    THRESHOLD_ABOVE_10 = 1u << 5, // 32
+    THRESHOLD_ABOVE_20 = 1u << 6, // 64
+    THRESHOLD_ABOVE_30 = 1u << 7, // 128
+    THRESHOLD_ABOVE_40 = 1u << 8, // 256
+    THRESHOLD_ABOVE_50 = 1u << 9, // 512
+    THRESHOLD_ABOVE_60 = 1u << 10, // 1024
+    THRESHOLD_ABOVE_70 = 1u << 11, // 2048
+    THRESHOLD_ABOVE_80 = 1u << 12, // 4096
+    THRESHOLD_ABOVE_90 = 1u << 13, // 8192
+    RATE_OF_CHANGE_SMALL = 1u << 14, // 16384
+    RATE_OF_CHANGE_MEDIUM = 1u << 15, // 32768
+    RATE_OF_CHANGE_LARGE = 1u << 16, // 65536
+    INTERNAL_FLAG_01 = 1u << 28, // 268435456
+    INTERNAL_FLAG_02 = 1u << 29, // 536870912
+    INTERNAL_FLAG_03 = 1u << 30, // 1073741824
+    INTERNAL_FLAG_04 = 1u << 31, // 2147483648
 } SensorFlags;
 
 // Dummy func

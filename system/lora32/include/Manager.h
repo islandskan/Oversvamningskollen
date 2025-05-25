ab9c1dfaa17141537177ccaf2f420dac6c9c813b
islandskan <sigridur.eggertsdottir@chasacademy.se>
2025-05-25 21:45:34 +0200

    remove incompatible low power/sleep library and implement fsptimer to aim for sort of sleep mode mock

--- system/lora32/include/Manager.h
+++ system/lora32/include/Manager.h
@@ -1,26 +1,31 @@
 #ifndef MANAGER_H
 #define MANAGER_H
-
 #include "../include/BatteryManager.h"
 #include "../include/SensorManager.h"
 #include "../include/TransmitterManager.h"
 // #include "../include/DisplayManager.h"
-#include "../lib/Mode.h"
+#include "../lib/config.h"
+#ifdef MOCK_MODE
+#include <FspTimer.h>
+#endif
 
 class Manager
 {
 private:
-    Mode _mode;
     SensorManager water_sensor;
     BatteryManager battery;
     TransmitterManager transmitter;
     // DisplayManager display;
-    void sleep_intervall(unsigned long duration_ms);
+#ifdef MOCK_MODE
+    FspTimer sleep_timer;
+#endif
 public:
-    Manager(Mode mode);
+    Manager();
     void cycle();
+#ifdef MOCK_MODE
+    void config_sleep_timer();
+    void sleep_interval(uint32_t duration_ms);
+#endif
 };
-
-
 
 #endif
--- system/lora32/src/Manager.cpp
+++ system/lora32/src/Manager.cpp
@@ -1,39 +1,84 @@
 #include "../include/Manager.h"
-#include <Arduino.h>
-#ifdef  ESP32
-#define MICROSECONDS_TO_SECONDS_FACTOR 1000ULL
-#else
-#include <ArduinoLowPower.h>
-#endif
 
-Manager::Manager(Mode mode) : _mode(mode), water_sensor(mode), battery(mode), transmitter(mode)
+volatile bool flag = false;
+volatile uint32_t sleep_counter = 0;
+uint32_t target = 0;
+
+Manager::Manager() : water_sensor(), battery(), transmitter()
 {
 }
 
-void Manager::sleep_intervall(unsigned long duration_ms) {
-#ifdef ESP32
-    esp_sleep_enable_timer_wakeup(duration_ms * MICROSECONDS_TO_SECONDS_FACTOR);
-    Serial.flush();
-    esp_deep_sleep_start();
-#else
-    delay(300);
-    for (int i = 0; i < duration_ms / 1000; ++i) {
-        LowPower.idle(1000);
+#ifdef MOCK_MODE
+void timer_callback(timer_callback_args_t *args) {
+    ++sleep_counter;
+    if (sleep_counter >= target) {
+        flag = true;
     }
-#endif
 }
+
+void Manager::config_sleep_timer() {
+    // float freq_hz = 1.0f / (duration_ms / 1000.0f);
+    float freq_hz = 1.0f;
+    uint8_t type;
+    // generally, fsptimer can't really handle low frequenzies(not without configurations), so atm just clamp the value if it's to low
+    int channel = FspTimer::get_available_timer(type);
+    if (channel == -1) {
+        Serial.println("no available timer");
+        return;
+    }
+    FspTimer::force_use_of_pwm_reserved_timer();
+    sleep_timer.begin(TIMER_MODE_ONE_SHOT, type, channel, freq_hz, 0.0f, timer_callback, nullptr);
+
+    if (!sleep_timer.setup_overflow_irq()) {
+        Serial.println("couldn't set up interrupts");
+        return;
+    }
+    if (!sleep_timer.open()) {
+        Serial.println("couldn't open the timer");
+        return;
+    }
+    if (!sleep_timer.start()) {
+        Serial.println("couldn't start the timer");
+        return;
+    }
+}
+
+void Manager::sleep_interval(uint32_t duration_ms) {
+    sleep_counter = 0;
+    flag = false;
+    target = duration_ms / 1000;
+    config_sleep_timer();
+
+    unsigned long start = millis();
+    while (!flag) {
+        delay(10);
+        if (millis() - start > duration_ms + 1000) {
+            break;
+        }
+    }
+    sleep_timer.stop();
+    sleep_timer.close();
+    Serial.println("Woke up from sleep");
+}
+
+#endif
+
+
 void Manager::cycle() {
-    Serial.println("Cycle complete. Going to sleep...");
+    // mostly to time the duration of the sleep
+    static uint32_t last_wake_time = millis();
+    uint32_t lap_time = millis() - last_wake_time;
+    Serial.print("Time since last wake time (seconds): ");
+    Serial.println(lap_time / 1000.0);
+    last_wake_time = millis();
     water_sensor.update();
     float level = water_sensor.get_water_level();
-    float change_of_rate = water_sensor.get_rate_of_change();
-    float voltage = battery.get_voltage();
+    float rate_of_change = water_sensor.get_rate_of_change();
     float percent = battery.get_battery();
-    String water_status = "Water level: " + String(level) + " cm | Change of rate: " + String(change_of_rate) + " %";
-    String battery_status = "Battery voltage: " + String(voltage, 2) + "V | Battery: " + String(percent) + " %";
-    Serial.println(water_status);
-    Serial.println(battery_status);
-    // Transmit data
+    transmitter.send(level, rate_of_change, percent);
     // Log to display
-    sleep_intervall(300000UL);
+    // sleep_interval(30000UL); // 30 seconds
+    sleep_interval(60000UL); // 1 minute
+    // sleep_interval(120000UL); // 2 minutes
+    // sleep_interval(300000UL); // 5 minutes
 }

57f4a6c42995166ea5f43d78b5fd7e03f55b68ef
islandskan <sigridur.eggertsdottir@chasacademy.se>
2025-05-26 12:19:24 +0200

    add convert_to_flags logic into the Transmitter class

--- system/lora32/include/TransmitterManager.h
+++ system/lora32/include/TransmitterManager.h
@@ -1,6 +1,10 @@
 #ifndef TRANSMITTERMANAGER_H
 #define TRANSMITTERMANAGER_H
+#include <Arduino.h>
+#include <HttpClient.h>
 #include "../lib/config.h"
+#include "../include/BatteryManager.h"
+#include "../include/SensorManager.h"
 // include utility function to encode the values into bit flags?
 #ifdef MOCK_MODE
 // includes for Serial, Wifi, HTTP etc
@@ -12,11 +16,35 @@
 class TransmitterManager
 {
 private:
+    const char* sensor_id;
+    uint32_t convert_to_flags(float battery, float water_level, float rate);
+    enum SensorFlags : uint32_t {
+        BATTERY_FULL = 1u << 0, // 1
+        BATTERY_MEDIUM = 1u << 1, // 2
+        BATTERY_LOW = 1u << 2, // 4
+        LOST_COMMUNICATION = 1u << 3, // 8
+        SENSOR_FAILURE = 1u << 4, // 16
+        THRESHOLD_ABOVE_10 = 1u << 5, // 32
+        THRESHOLD_ABOVE_20 = 1u << 6, // 64
+        THRESHOLD_ABOVE_30 = 1u << 7, // 128
+        THRESHOLD_ABOVE_40 = 1u << 8, // 256
+        THRESHOLD_ABOVE_50 = 1u << 9, // 512
+        THRESHOLD_ABOVE_60 = 1u << 10, // 1024
+        THRESHOLD_ABOVE_70 = 1u << 11, // 2048
+        THRESHOLD_ABOVE_80 = 1u << 12, // 4096
+        THRESHOLD_ABOVE_90 = 1u << 13, // 8192
+        RATE_OF_CHANGE_SMALL = 1u << 14, // 16384
+        RATE_OF_CHANGE_MEDIUM = 1u << 15, // 32768
+        RATE_OF_CHANGE_LARGE = 1u << 16, // 65536
+        INTERNAL_FLAG_01 = 1u << 28, // 268435456
+        INTERNAL_FLAG_02 = 1u << 29, // 536870912
+        INTERNAL_FLAG_03 = 1u << 30, // 1073741824
+        INTERNAL_FLAG_04 = 1u << 31, // 2147483648
+    };
 
 public:
-    TransmitterManager();
-    void begin();
-    void send(float level, float rate, float battery);
+    TransmitterManager(const char* id);
+    void send_data(BatteryManager& battery, SensorManager& sensor);
 
 };
 
--- system/lora32/src/TransmitterManager.cpp
+++ system/lora32/src/TransmitterManager.cpp
@@ -1,26 +1,79 @@
 #include "../include/TransmitterManager.h"
-#include <Arduino.h>
+#include <ArduinoJson.h>
 
-TransmitterManager::TransmitterManager()
+#define SERVER_URL ""
+#define API_KEY ""
+
+TransmitterManager::TransmitterManager(const char* id) : sensor_id(id)
 {
+    // #if MOCK_MODE
+
+    //     // test and set up
+    // #else
+    //     // lora
+    // // test and set up
+    // #endif
 }
 
-void TransmitterManager::begin()
-{
+void TransmitterManager::send_data(BatteryManager& battery, SensorManager& sensor) {
 #if MOCK_MODE
-    // Serial or Wifi
-// test and set up
-#else
-    // lora
-// test and set up
-#endif
-}
-
-void TransmitterManager::send(float level, float rate, float battery) {
-#if MOCK_MODE
-    String data = "[MOCK] Transmitting data: \nWater Level: " + String(level) + " | Rate: " + String(rate) + " | Battery percentage: " + String(battery);
-    Serial.println(data);
+    //
 #else
     // LoRa packet sending
 #endif
+}
+
+uint32_t TransmitterManager::convert_to_flags(float battery, float water_level, float rate) {
+    uint32_t flags = 0;
+
+    if (battery >= 80.0f) {
+        flags |= BATTERY_FULL;
+    }
+    else if (battery >= 20.0f) {
+        flags |= BATTERY_MEDIUM;
+    }
+    else {
+        flags |= BATTERY_LOW;
+    }
+
+    if (water_level > 90.0f) {
+        flags |= THRESHOLD_ABOVE_90;
+    }
+    else if (water_level > 80.0) {
+        flags |= THRESHOLD_ABOVE_80;
+    }
+    else if (water_level > 70.0) {
+        flags |= THRESHOLD_ABOVE_70;
+    }
+    else if (water_level > 60.0) {
+        flags |= THRESHOLD_ABOVE_60;
+    }
+    else if (water_level > 50.0) {
+        flags |= THRESHOLD_ABOVE_50;
+    }
+    else if (water_level > 40.0) {
+        flags |= THRESHOLD_ABOVE_40;
+    }
+    else if (water_level > 30.0) {
+        flags |= THRESHOLD_ABOVE_30;
+    }
+    else if (water_level > 20.0) {
+        flags |= THRESHOLD_ABOVE_20;
+    }
+    else if (water_level > 10.0) {
+        flags |= THRESHOLD_ABOVE_10;
+    }
+
+    if (rate > 10.0f) {
+        flags |= RATE_OF_CHANGE_LARGE;
+    }
+    else if (rate > 5.0f) {
+        flags |= RATE_OF_CHANGE_MEDIUM;
+    }
+    else if (rate > 1.0f) {
+        flags |= RATE_OF_CHANGE_SMALL;
+    }
+    Serial.println(flags, BIN);
+    // other values should also be LOST_COMMUNICATION, SENSOR_FAILURE, RATE_OF_CHANGE_SMALL, RATE_OF_CHANGE_MEDIUM, RATE_OF_CHANGE_LARGE
+    return flags;
 }

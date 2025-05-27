090a02b9f7194371b8437445c9cd29e5384967ba
islandskan <sigridur.eggertsdottir@chasacademy.se>
2025-05-27 10:53:10 +0200

    update configs and add example config for wifi tokens

---
+++ system/lora32/lib/transmit_config.example.h
@@ -0,0 +1,11 @@
+#ifndef TRANSMIT_CONFIG_H
+#define TRANSMIT_CONFIG_H
+#include <Arduino.h> // when it's included into your project, comment out this. Just for the uint16_t
+
+constexpr char* URL_PATH = "Your url path";
+constexpr const char* SERVER_NAME = "Your IP ADDRESS";
+constexpr uint16_t PORT = 1; // change to your port as an integer
+constexpr const char* API_KEY = "Your api key";
+constexpr const char* SSID = "Your WiFi SSID";
+constexpr const char* PASSWORD = "Your WiFi password";
+#endif

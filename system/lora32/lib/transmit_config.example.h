b238715d71c7a664deb5e3a31c8cdda6c06d4f4c
islandskan <sigridur.eggertsdottir@chasacademy.se>
2025-06-09 09:35:59 +0200

    fix typos and remove unused parts

--- system/lora32/lib/transmit_config.example.h
+++ system/lora32/lib/transmit_config.example.h
@@ -2,10 +2,10 @@
 #define TRANSMIT_CONFIG_H
 #include <Arduino.h> // when it's included into your project, comment out this. Just for the uint16_t
 
-#define URL_PATH = "Your url path";
-#define SERVER_NAME = "Your IP ADDRESS";
-#define PORT = 1; // change to your port as an integer
-#define API_KEY = "Your api key";
-#define SSID = "Your WiFi SSID";
-#define PASSWORD = "Your WiFi password";
+#define URL_PATH "Your url path";
+#define SERVER_NAME "Your IP ADDRESS";
+#define PORT 1; // change to your port as an integer
+#define API_KEY "Your api key";
+#define SSID "Your WiFi SSID";
+#define PASSWORD "Your WiFi password";
 #endif
